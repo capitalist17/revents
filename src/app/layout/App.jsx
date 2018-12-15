3eea50c59401bfb8f1ce4655872c755c73d1f085
Arvind SN <asn@asn.com>
2018-12-15 13:43:55 +0530

    Adding the Navigation Bar and Styling

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
-import {Button} from 'semantic-ui-react';
+import { Container } from 'semantic-ui-react';
 import EventDashboard from '../../features/events/EventDashboard/EventDashboard';
+import NavBar from '../../features/nav/NavBar/NavBar';
 class App extends Component {
   render() {
     return (
       <div>
-        <h1>Re-vents</h1>
-        <EventDashboard/>
-      </div>
+        <NavBar />
+        <Container className="main">          
+          <EventDashboard />
+        </Container>
+      </div>      
     );
   }
 }
