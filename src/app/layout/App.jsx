2bb998476072f96c16e8c4e9d55254c2ec0ecbef
Arvind SN <asn@asn.com>
2018-12-18 10:34:58 +0530

    Route Config Continued and Adding a homepage

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import { Container } from 'semantic-ui-react';
 import HomePage from '../../features/home/HomePage';
 import EventDashboard from '../../features/events/EventDashboard/EventDashboard';
@@ -14,18 +14,28 @@ class App extends Component {
   render() {
     return (
       <div>
-        <NavBar />
-        <Container className="main">          
-          {/* <EventDashboard /> */}
+        <Switch>
           <Route exact path='/' component={HomePage} />
-          <Route path='/events' component={EventDashboard} />
-          <Route path='/event/:id' component={EventDetailedPage} />
-          <Route path='/people' component={PeopleDashboard} />
-          <Route path='/profile/:id' component={UserDetailedPage} />
-          <Route path='/settings' component={SettingsDashboard} />
-          <Route path='/createEvent' component={EventForm} />          
-        </Container>
-      </div>      
+        </Switch>
+
+        <Route path='/(.+)' render = { () => {
+          return (<div>
+                    <NavBar />
+                    <Container className="main">          
+                      {/* <EventDashboard /> */}
+                      <Switch>
+                        <Route path='/events' component={EventDashboard} />
+                        <Route path='/event/:id' component={EventDetailedPage} />
+                        <Route path='/people' component={PeopleDashboard} />
+                        <Route path='/profile/:id' component={UserDetailedPage} />
+                        <Route path='/settings' component={SettingsDashboard} />
+                        <Route path='/createEvent' component={EventForm} />        
+                      </Switch>  
+                    </Container>
+                  </div> )
+        }}/>
+         
+      </div>  
     );
   }
 }
