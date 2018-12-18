a0707ced0e4bfb48addf809e87ba946ff31ea31a
Arvind SN <asn@asn.com>
2018-12-18 11:13:26 +0530

    Adding SignIn and SignOut menus to Navbar

--- src/features/nav/NavBar/NavBar.jsx
+++ src/features/nav/NavBar/NavBar.jsx
@@ -1,24 +1,40 @@
 import React, { Component } from 'react'
 import { NavLink, Link } from 'react-router-dom'
 import { Menu, Button, Container } from 'semantic-ui-react';
+import SignedOutMenu from '../Menus/SignedOutMenu';
+import SignedInMenu from '../Menus/SignedInMenu';
+
 class NavBar extends Component {
+  state = {authenticated:true}
+
+  handleSignIn = () => {
+    this.setState({ authenticated: true });
+  };
+
+  handleSignOut = () => {
+    this.setState({ authenticated: false });
+    //this.props.history.push('/');
+  };
+
   render() {
+    const {authenticated} = this.state;
     return (
         <Menu inverted fixed="top">
             <Container>
                 <Menu.Item header as={Link} to="/">
                     <img src="assets/logo.png" alt="logo" />
                     Re-vents
-            </Menu.Item>
+                </Menu.Item>
+
                 <Menu.Item as={NavLink} to="/events" name="Events" />
                 <Menu.Item as={NavLink} to="/people" name="People" />
+
                 <Menu.Item>
                     <Button as={Link} to="/createEvent" floated="right" positive inverted content="Create Event" />
                 </Menu.Item>
-                <Menu.Item position="right">
-                    <Button basic inverted content="Login" />
-                    <Button basic inverted content="Sign Out" style={{ marginLeft: '0.5em' }} />
-                </Menu.Item>
+                {authenticated ? <SignedInMenu signOut={this.handleSignOut}/> 
+                               : <SignedOutMenu signIn={this.handleSignIn} /> }
+                
             </Container>
         </Menu>
     )
