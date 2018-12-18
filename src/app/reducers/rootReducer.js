e6b19d3369246efd8985094dfd1ff765f0538970
Arvind SN <asn@asn.com>
2018-12-18 17:08:47 +0530

    Creating our first actions

--- src/app/reducers/rootReducer.js
+++ src/app/reducers/rootReducer.js
@@ -1,6 +1,6 @@
 // The rootReducer is going to combine multiple reducers and pass it on to store
 import { combineReducers } from 'redux';
-import testReducer from './testReducer';
+import testReducer from '../../features/testarea/testReducer';
 
 
 const rootReducer = combineReducers({
--- src/features/testarea/testReducer.js
+++ src/features/testarea/testReducer.js
@@ -1,30 +1,32 @@
 import { INCREMENT_COUNTER, DECREMENT_COUNTER } from './testConstants';
-import { createReducer } from '../../app/common/util/reducerUtil'
+// import { createReducer } from '../../app/common/util/reducerUtil'
 
 const initialState = {
-  data: 43
+  data: 45
 };
 
-export const incrementCounter = (state, payload) => {
-  return { ...state, data: state.data + 1 };
-}
+const testReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case INCREMENT_COUNTER:
+      return { ...state, data: state.data + 1 };
+    case DECREMENT_COUNTER:
+      return { ...state, data: state.data - 1 };
+    default:
+      return state;
+  }
+};
 
-export const decrementCounter = (state, payload) => {
-  return { ...state, data: state.data - 1 };
-}
+export default testReducer;
 
-// const testReducer = (state = initialState, action) => {
-//   switch (action.type) {
-//     case INCREMENT_COUNTER:
-//       return { ...state, data: state.data + 1 };
-//     case DECREMENT_COUNTER:
-//       return { ...state, data: state.data - 1 };
-//     default:
-//       return state;
-//   }
-// };
+// export const incrementCounter = (state, payload) => {
+//   return { ...state, data: state.data + 1 };
+// }
 
-export default createReducer(initialState, {
-  [INCREMENT_COUNTER]: incrementCounter,
-  [DECREMENT_COUNTER]: decrementCounter
-});
+// export const decrementCounter = (state, payload) => {
+//   return { ...state, data: state.data - 1 };
+// }
+
+// export default createReducer(initialState, {
+//   [INCREMENT_COUNTER]: incrementCounter,
+//   [DECREMENT_COUNTER]: decrementCounter
+// });
