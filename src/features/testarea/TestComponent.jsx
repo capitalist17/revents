5f7b66e171c0036a8a6c342cccf18e9cbb2bc44c
Arvind SN <asn@asn.com>
2018-12-21 11:16:20 +0530

    Setting up places autocomplet in Google maps

--- src/features/testarea/TestComponent.jsx
+++ src/features/testarea/TestComponent.jsx
@@ -1,6 +1,9 @@
 import React, { Component } from 'react'
 import { Button } from 'semantic-ui-react'
 import { connect } from 'react-redux'; // using this we can bind this component to the redux store
+import Script from 'react-load-script';
+import PlacesAutocomplete, { geocodeByAddress, getLatLng } from 'react-places-autocomplete';
+
 import { incrementCounter, decrementCounter } from './testActions'
 
 const mapStateToProps = (state) => ({
@@ -13,14 +16,43 @@ const mapDispatchToProps = {
 }
 
 class TestComponent extends Component {
+  state = { address: '', 
+            scriptLoaded: false }
+
+  handleScriptLoad = () => {
+    this.setState({scriptLoaded:true})
+  }
+  handleFormSubmit = (event) => {
+    event.preventDefault()
+
+    geocodeByAddress(this.state.address)
+      .then(results => getLatLng(results[0]))
+      .then(latLng => console.log('Success', latLng))
+      .catch(error => console.error('Error', error))
+  }
+  
+  onChange = (address) => this.setState({address})
+
   render() {
+    const inputProps = {
+      value: this.state.address,
+      onChange: this.onChange,
+    }
     const {incrementCounter, decrementCounter, data} = this.props;
     return (
       <div>
+        <Script 
+        url="https://maps.googleapis.com/maps/api/js?key=AIzaSyD9EdV2JfPG1Vfviw9gf_HlblIUfs7Ie2E&libraries=places" 
+        onLoad = {this.handleScriptLoad}/>
         <h1>Test Area</h1>
         <h3>The answer is: {data}</h3>
         <Button onClick={incrementCounter} color='green' content='Increment' />
         <Button onClick={decrementCounter} color='red' content='Decrement' />
+        <br/> <br/> <br/> <br/>
+        <form onSubmit={this.handleFormSubmit}>
+          {this.state.scriptLoaded && <PlacesAutocomplete inputProps={inputProps} /> }          
+          <button type="submit">Submit</button>
+        </form>
       </div>
     )
   }
