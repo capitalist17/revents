dc2b0229f4da4fef544f2683ffa5c52cfe5ac927
Arvind SN <asn@asn.com>
2018-12-27 16:27:02 +0530

    Setting up the BasicPage form

--- src/features/user/Settings/SettingsDashboard.jsx
+++ src/features/user/Settings/SettingsDashboard.jsx
@@ -12,7 +12,8 @@ import { updatePassword } from '../../auth/authActions';
 
 const mapStateToProps = (state) => {
   return {
-    providerId: state.firebase.auth.providerData[0].providerId
+    providerId: state.firebase.auth.providerData[0].providerId,
+    user: state.firebase.profile
   }
 }
 
@@ -20,15 +21,18 @@ const mapDispatchToProps = {
   updatePassword
 }
 
-const SettingsDashboard = ({providerId, updatePassword}) => {
+// all the state information as well as action information is passed to this component as props parameter
+const SettingsDashboard = ({providerId, updatePassword, user}) => {
   return (
     <Grid>
       <Grid.Column width={12}>
         <Switch>
           <Redirect exact from='/settings' to='/settings/basic' />
-          <Route path='/settings/basic' component={BasicPage}></Route>
+          {/* If you want the fields to be populated, then use initialvalues */}
+          <Route path='/settings/basic' render={() => <BasicPage initialValues={user} />} ></Route>
           <Route path='/settings/about' component={AboutPage}></Route>
           <Route path='/settings/photos' component={PhotosPage}></Route>
+          {/* In case of AccountPage n initialvalues had to be populated as its a password field */}
           <Route path='/settings/account' 
                  render={() => <AccountPage providerId={providerId} 
                                             updatePassword={updatePassword} />} >
