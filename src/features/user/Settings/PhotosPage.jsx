92ca087dbec1cfbe8f74b0fc295ef49a35a74bcf
Arvind SN <asn@asn.com>
2018-12-28 18:09:42 +0530

    Displaying photos on the page

--- src/features/user/Settings/PhotosPage.jsx
+++ src/features/user/Settings/PhotosPage.jsx
@@ -12,7 +12,8 @@ import { uploadProfileImage } from '../userActions';
 
 const mapStateToProps = state => ({
   auth: state.firebase.auth,
-  profile: state.firebase.profile
+  profile: state.firebase.profile,
+  photos: state.firestore.ordered.photos
 });
 
 const mapDispatchToProps = {
@@ -76,70 +77,80 @@ class PhotosPage extends Component {
     };
 
     render() {
-        return (
-            <Segment>
-                <Header dividing size='large' content='Your Photos' />
-                <Grid>
-                    <Grid.Row />
-                    <Grid.Column width={4}>
-                        <Header color='teal' sub content='Step 1 - Add Photo'/>
-                        <Dropzone onDrop={this.onDrop} multiple={false}>
-                          <div style={{ paddingTop: '30px', textAlign: 'center' }}>
-                            <Icon name="upload" size="huge" />
-                            <Header content="Drop image here or click to upload" />
-                          </div>
-                        </Dropzone>
-                    </Grid.Column>
-                    <Grid.Column width={1} />
-                    <Grid.Column width={4}>
-                        <Header sub color='teal' content='Step 2 - Resize image' />
-                        {this.state.files[0] && (
-                                            <Cropper style={{ height: 200, width: '100%' }}
-                                              ref="cropper" src={this.state.files[0].preview}
-                                              aspectRatio={1} viewMode={0} dragMode="move"
-                                              guides={false} scalable={true} cropBoxMovable={true}
-                                              cropBoxResizable={true} crop={this.cropImage}
-                                            />
-                                                )}
-                    </Grid.Column>
-                    <Grid.Column width={1} />
-                    <Grid.Column width={4}>
-                        <Header sub color='teal' content='Step 3 - Preview and Upload' />
-                        {this.state.files[0] &&
-                        <div>
-                          <Image style={{ minHeight: '200px', minWidth: '200px' }}
-                                 src={this.state.cropResult} />
-                          <Button.Group>
-                            <Button onClick={this.uploadImage} 
-                                    style={{ width: '100px' }} positive icon="check" />
-                            <Button onClick={this.cancelCrop} 
-                                    style={{ width: '100px' }} icon="close" />
-                          </Button.Group>
+      const { photos, profile } = this.props;
+      let filteredPhotos;
+      if (photos) {
+          filteredPhotos = photos.filter(photo => {
+          return photo.url !== profile.photoURL
+        })
+      }
+
+      return (
+          <Segment>
+              <Header dividing size='large' content='Your Photos' />
+              <Grid>
+                  <Grid.Row />
+                  <Grid.Column width={4}>
+                      <Header color='teal' sub content='Step 1 - Add Photo'/>
+                      <Dropzone onDrop={this.onDrop} multiple={false}>
+                        <div style={{ paddingTop: '30px', textAlign: 'center' }}>
+                          <Icon name="upload" size="huge" />
+                          <Header content="Drop image here or click to upload" />
                         </div>
-                       }
-                    </Grid.Column>
+                      </Dropzone>
+                  </Grid.Column>
+                  <Grid.Column width={1} />
+                  <Grid.Column width={4}>
+                      <Header sub color='teal' content='Step 2 - Resize image' />
+                      {this.state.files[0] && (
+                                          <Cropper style={{ height: 200, width: '100%' }}
+                                            ref="cropper" src={this.state.files[0].preview}
+                                            aspectRatio={1} viewMode={0} dragMode="move"
+                                            guides={false} scalable={true} cropBoxMovable={true}
+                                            cropBoxResizable={true} crop={this.cropImage}
+                                          />
+                                              )}
+                  </Grid.Column>
+                  <Grid.Column width={1} />
+                  <Grid.Column width={4}>
+                      <Header sub color='teal' content='Step 3 - Preview and Upload' />
+                      {this.state.files[0] &&
+                      <div>
+                        <Image style={{ minHeight: '200px', minWidth: '200px' }}
+                                src={this.state.cropResult} />
+                        <Button.Group>
+                          <Button onClick={this.uploadImage} 
+                                  style={{ width: '100px' }} positive icon="check" />
+                          <Button onClick={this.cancelCrop} 
+                                  style={{ width: '100px' }} icon="close" />
+                        </Button.Group>
+                      </div>
+                      }
+                  </Grid.Column>
 
-                </Grid>
+              </Grid>
 
-                <Divider/>
-                <Header sub color='teal' content='All Photos'/>
+              <Divider/>
+              <Header sub color='teal' content='All Photos'/>
 
-                <Card.Group itemsPerRow={5}>
-                    <Card>
-                        <Image src='https://randomuser.me/api/portraits/men/20.jpg'/>
-                        <Button positive>Main Photo</Button>
-                    </Card>
-
-                    <Card >
-                        <Image src='https://randomuser.me/api/portraits/men/20.jpg' />
-                        <div className='ui two buttons'>
+              <Card.Group itemsPerRow={5}>
+                  <Card>
+                      <Image src={profile.photoURL  || '/assets/user.png'}/>
+                      <Button positive>Main Photo</Button>
+                  </Card>
+                  {photos &&
+                      filteredPhotos.map(photo => (
+                        <Card key={photo.id}>
+                          <Image src={photo.url} />
+                          <div className="ui two buttons">
                             <Button basic color='green'>Main</Button>
                             <Button basic icon='trash' color='red' />
-                        </div>
-                    </Card>
-                </Card.Group>
-            </Segment>
-        );
+                          </div>
+                        </Card>
+                      ))}
+              </Card.Group>
+          </Segment>
+      );
     }
 }
 
