61a8667cb94d9b8aefcc76627a0b583911774b45
Arvind SN <asn@asn.com>
2019-01-01 11:28:08 +0530

    React lazy load

--- src/features/user/UserDetailed/UserDetailedPhotos.jsx
+++ src/features/user/UserDetailed/UserDetailedPhotos.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Grid, Header, Image, Segment } from 'semantic-ui-react';
+import LazyLoad from 'react-lazyload';
 
 const UserDetailedPhotos = ({photos}) => {
   return (
@@ -8,7 +9,15 @@ const UserDetailedPhotos = ({photos}) => {
       <Header icon="image" content="Photos" />
 
       <Image.Group size="small">
-        {photos && photos.map(photo => <Image key={photo.id} src={photo.url} />)}
+        {photos && 
+          photos.map(photo => 
+            // Aside, use offset={-150} to show the images only when it reaches the viewport of 150
+            // In other word, display images when the user scrolls down to that point where images
+            // are placed inside the page.
+            <LazyLoad key={photo.id} height={150} placeholder={<Image src='/assets/user.png' />}>
+              <Image src={photo.url} />
+            </LazyLoad>
+          )}
       </Image.Group>
     </Segment>
   </Grid.Column>
