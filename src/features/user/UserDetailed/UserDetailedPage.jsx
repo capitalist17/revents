c42bb513881ef9a8882fc702ab472f73e599284d
Arvind SN <asn@asn.com>
2019-01-02 12:38:56 +0530

    Adding Tab functionallity to change the query and end of section 20

--- src/features/user/UserDetailed/UserDetailedPage.jsx
+++ src/features/user/UserDetailed/UserDetailedPage.jsx
@@ -42,6 +42,10 @@ class UserDetailedPage extends Component {
     console.log(events)
   }
 
+  changeTab = (e, data) => {
+    this.props.getUserEvents(this.props.userUid, data.activeIndex)
+  }
+
   render() {
     const {profile, photos, auth, match, requesting, events, eventsLoading} = this.props;
     const isCurrentUser = auth.uid === match.params.id;
@@ -56,7 +60,7 @@ class UserDetailedPage extends Component {
         <UserDetailedSidebar isCurrentUser= {isCurrentUser}/>
         {photos && photos.length > 0 &&
         <UserDetailedPhotos photos={photos}/>}
-        <UserDetailedEvents events={events} eventsLoading={eventsLoading} />
+        <UserDetailedEvents events={events} eventsLoading={eventsLoading} changeTab={this.changeTab}/>
       </Grid>
     );
   }
